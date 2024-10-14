6adb50cc2664b8a97db0ad19fb0bbe9968a8eeef
Aaryaneil Nimbalkar <aaryaneil.nimbalkar@gmail.com>
2024-10-14 14:55:22 -0700

    Task 1, 2, 3 commit.

--- LoginSystem/LoginSystem/settings.py
+++ LoginSystem/LoginSystem/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
